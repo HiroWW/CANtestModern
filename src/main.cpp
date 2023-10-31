ebeeb1f1f4845abdf2444924fade979c27691bbe
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-10-31 19:27:10 +0900

    kaitayo

--- src/main.cpp
+++ src/main.cpp
@@ -3,117 +3,102 @@
 #include "CANpack.hpp"
 #include <vector>
 #include <unordered_map>
-//--------------------------------------
-//   change here to switch read/send
-//--------------------------------------
-bool IFREAD = true;
-bool IFDEBUG = true;
-//--------------------------------------
 
 CANpack canpack;
 
 void setup() {
     Serial.println("Waiting for setup...");
-    int ids[] = {CAN_ID_CENTERTOMASTER};
-    canpack.CANsetup(ids,sizeof(ids));
+    canpack.CANsetup();
     Serial.println("CAN setup : COMPLETE");
 }
 
-int loopCount = 0;
+#include <iostream>
+
+// フラグの定義
+#define MASTER1 1
+#define MASTER2 2
+#define IF 3
+#define LEFT 4
+#define CENTER 5
+#define RIGHT 6
+
+
+
+// ================== ここでフラグを設定 ==================
+
+
+
+
+
+
+
+
+#define CURRENT_FLAG LEFT
+
+
+
+
+
+
+
+
+// =======================================================
+
+
+
 void loop() {
-
-    //--------------------------------------------------------------------------------------------------
-    
-    // setup Master to Tail pack and send it to CAN bus
-    canMasterToLeft.updateTime = 1.1f * (loopCount % 5 + 1);
-    canMasterToLeft.drCommand = 11.11f * (loopCount % 5 + 1);
-    canMasterToLeft.deCommand = 111.111f * (loopCount % 5 + 1);
-    for (int i = 0; i < 15; i++){
-        canMasterToLeft.err_state[i] = 1.0f * i;
+    // フラグに応じた処理
+    switch (CURRENT_FLAG) {
+        case MASTER1:
+            // MASTER1の場合の処理
+            // Serial.println("Executing MASTER1 logic...");
+            canMasterToIF.receive_state = 1;
+            canpack.CANsend(CAN_ID_MASTERTOIF, &canMasterToIF);
+            canpack.CANread({CAN_ID_MASTERTOCENTER, CAN_ID_IFTOMASTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
+            break;
+        case MASTER2:
+            // MASTER2の場合の処理
+            // Serial.println("Executing MASTER2 logic...");
+            canMasterToCenter.receive_state = 1;
+            canpack.CANsend(CAN_ID_MASTERTOCENTER, &canMasterToCenter);
+            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_IFTOMASTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
+            break;
+        case IF:
+            // IFの場合の処理
+            // Serial.println("Executing IF logic...");
+            canIFtoMaster.receive_state = 1;
+            canpack.CANsend(CAN_ID_IFTOMASTER,&canIFtoMaster);
+            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
+            break;
+        case LEFT:
+            // LEFTの場合の処理
+            // Serial.println("Executing LEFT logic...");
+            canLeftToMaster.receive_state = 1;
+            canpack.CANsend(CAN_ID_RIGHTTOMASTER, &canLeftToMaster);
+            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_IFTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
+            break;
+        case CENTER:
+            // CENTERの場合の処理
+            // Serial.println("Executing CENTER logic...");
+            canCenterToMaster.receive_state = 1;
+            canpack.CANsend(CAN_ID_CENTERTOMASTER, &canCenterToMaster);
+            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_IFTOMASTER, CAN_ID_RIGHTTOMASTER});
+            break;
+        case RIGHT:
+            // RIGHTの場合の処理
+            // Serial.println("Executing RIGHT logic...");
+            canRightToMaster.receive_state = 1;
+            canpack.CANsend(CAN_ID_RIGHTTOMASTER, &canRightToMaster);
+            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_IFTOMASTER});
+            break;
+        default:
+            // 定義されていないフラグの場合の処理
+            Serial.println("Unknown flag!");
+            break;
     }
-    canMasterToLeft.gravity[0] = 33.4f;
-    canMasterToLeft.gravity[1] = 66.8f;
-    canMasterToLeft.gravity[2] = 99.4f;
-    canMasterToLeft.mode = 2.0f * (loopCount % 5 + 1);
-    canMasterToLeft.receive_state = true;
-    canpack.CANsend(CAN_ID_MASTERTORIGHT ,&canMasterToLeft);
 
-    canLeftToMaster.acc[0] =  1.1f * (loopCount % 5 + 1);
-    canLeftToMaster.gyro[2] =  111.111f * (loopCount % 5 + 1);
-    canLeftToMaster.heading = 33.44f;
-    canpack.CANsend(CAN_ID_CENTERTOMASTER,&canLeftToMaster);
-    // canpack.CANsend(CAN_ID_MASTERTORIGHT ,&canMasterToLeft);
-    // canpack.CANsend(CAN_ID_MASTERTORIGHT ,&canMasterToLeft);
-
-    UTHAPS::println("TX SIZE",sizeof(canMasterToLeft));
-
-    //--------------------------------------------------------------------------------------------------
-
-    canpack.CANread({CAN_ID_CENTERTOMASTER});
-
-    if (IFDEBUG){
-        // UTHAPS::println("---------- 0 : Master To IF content----------");
-        // UTHAPS::println("attituded_dt = ",mip.attitude_dt);
-        // UTHAPS::println("main_dt = ", mip.main_dt);
-        // UTHAPS::println("control_dt = ", mip.control_dt);
-
-        UTHAPS::println("");
-        UTHAPS::println("---------- 1 :Tail To Master content----------");
-        UTHAPS::println("acc 0", canCenterToMaster.acc[0]);
-        UTHAPS::println("gyro 2", canCenterToMaster.gyro[2]);
-        UTHAPS::println("heading ", canCenterToMaster.heading);
-
-        UTHAPS::println("---------- 2 : Master to Tail content----------");
-        UTHAPS::println("updateTime = ", canMasterToRight.updateTime);
-        UTHAPS::println("drCommand = ", canMasterToRight.drCommand);
-        UTHAPS::println("deCommand = ", canMasterToRight.deCommand);
-        UTHAPS::println("err state = ", canMasterToRight.err_state[1]);
-        UTHAPS::println("gravity = ", canMasterToRight.gravity[2]);
-        UTHAPS::println("canMasterToRight.mode = ", canMasterToRight.mode);
-
-        UTHAPS::println("---------- EX : loop CNT ----------");
-
-        UTHAPS::println("RX loop count is ",(canMasterToRight.updateTime / 11.1f - 1));
-        UTHAPS::println("TX loop count is ", loopCount);
-    }
-    if (! IFREAD){
-        CANMessage message;
-        UTHAPS::print("receive data ");
-        for (int i = 0; i < 12; i++){
-            if (ACAN_T4::can2.receive(message)){
-                UTHAPS::println("id", message.id);
-                for (int i = 0; i < 8; i++){
-                    // int dataCon = message.data[]
-                    UTHAPS::print(message.data[i], " ");
-                }
-                UTHAPS::println("");
-            }
-        }
-    }
-    loopCount++ ;
-    if (loopCount == 5){
-        loopCount = 0;
-    }
+    // 現在のフラグをシリアルで出力
+    Serial.print("Current board is: ");
+    Serial.print(CURRENT_FLAG);
+    UTHAPS::println("M1 >> ", canMasterToIF.receive_state, "M2 >> ", canMasterToCenter.receive_state, "IF >> ", canIFtoMaster.receive_state, "LEFT >> ", canLeftToMaster.receive_state, "CENTER >> ", canCenterToMaster.receive_state, "RIGHT >> ", canRightToMaster.receive_state);
 }
-
-// struct -> union OK
-// union -> cobs OK
-// cobs -> cutarr OK
-    // while文するとおかしくなる。
-// buffer -> msgdata NG
-    // msg dataを切り抜く処理がおかしいよよね～～
-    // indexを変えるとmsgdataとdataEncodedの値は一致する
-    // でもcobsにぶちこむとフリーズする　うんこ
-// msgdata -> decoded data OK
-// decoded data -> cobsINF ??
-
-/// decoded_dataをていぎすると、message dataの最初に0がはいってしまい、cobsがうまくいかなくなってフリーズする　という仕組み。
-
-// // master
-// IF,L,C,R
-
-// // IF
-// Master , L,C,R
-
-// // Tail
-// Master 

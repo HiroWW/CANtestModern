504bc1c49d2df02b77d7af5b86482aebf08f6bbf
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-11-02 21:53:53 +0900

    だいぶいいかんじやとおもう

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,44 @@
 
 CANpack canpack;
 
+
+const int extra_buffer_size = 512;
+
+uint8_t extra_read_buffer_1[extra_buffer_size];
+uint8_t extra_read_buffer_2[extra_buffer_size];
+uint8_t extra_read_buffer_3[extra_buffer_size];
+uint8_t extra_read_buffer_4[extra_buffer_size];
+uint8_t extra_read_buffer_5[extra_buffer_size];
+uint8_t extra_read_buffer_6[extra_buffer_size];
+uint8_t extra_read_buffer_7[extra_buffer_size];
+uint8_t extra_read_buffer_8[extra_buffer_size];
+uint8_t extra_write_buffer_1[extra_buffer_size];
+uint8_t extra_write_buffer_2[extra_buffer_size];
+uint8_t extra_write_buffer_3[extra_buffer_size];
+uint8_t extra_write_buffer_4[extra_buffer_size];
+uint8_t extra_write_buffer_5[extra_buffer_size];
+uint8_t extra_write_buffer_6[extra_buffer_size];
+uint8_t extra_write_buffer_7[extra_buffer_size];
+uint8_t extra_write_buffer_8[extra_buffer_size];
+
+
 void setup() {
+    Serial1.addMemoryForRead(extra_read_buffer_1, extra_buffer_size);
+    Serial2.addMemoryForRead(extra_read_buffer_2, extra_buffer_size);
+    Serial3.addMemoryForRead(extra_read_buffer_3, extra_buffer_size);
+    Serial4.addMemoryForRead(extra_read_buffer_4, extra_buffer_size);
+    Serial5.addMemoryForRead(extra_read_buffer_5, extra_buffer_size);
+    Serial6.addMemoryForRead(extra_read_buffer_6, extra_buffer_size);
+    Serial7.addMemoryForRead(extra_read_buffer_7, extra_buffer_size);
+    Serial8.addMemoryForRead(extra_read_buffer_8, extra_buffer_size);
+    Serial1.addMemoryForWrite(extra_write_buffer_1, extra_buffer_size);
+    Serial2.addMemoryForWrite(extra_write_buffer_2, extra_buffer_size);
+    Serial3.addMemoryForWrite(extra_write_buffer_3, extra_buffer_size);
+    Serial4.addMemoryForWrite(extra_write_buffer_4, extra_buffer_size);
+    Serial5.addMemoryForWrite(extra_write_buffer_5, extra_buffer_size);
+    Serial6.addMemoryForWrite(extra_write_buffer_6, extra_buffer_size);
+    Serial7.addMemoryForWrite(extra_write_buffer_7, extra_buffer_size);
+    Serial8.addMemoryForWrite(extra_write_buffer_8, extra_buffer_size);
     Serial.println("Waiting for setup...");
     canpack.CANsetup();
     Serial.println("CAN setup : COMPLETE");
@@ -102,26 +139,26 @@ void loop() {
     Serial.print("--");
     UTHAPS::println("=======================================================================================================================================");
     // 現在のフラグの名前をシリアルで出力
-    UTHAPS::print("Current flag is: ");
+    Serial.print("Current flag is: ");
     switch (CURRENT_FLAG) {
         case MASTER1: UTHAPS::print("MASTER1"); break;
         case MASTER2: UTHAPS::print("MASTER2"); break;
         case IF: UTHAPS::print("IF"); break;
         case LEFT: UTHAPS::print("LEFT"); break;
         case CENTER: UTHAPS::print("CENTER"); break;
-        case RIGHT: UTHAPS::print("RIGHT"); break;
+        case RIGHT: Serial.print("RIGHT"); break;
         default: UTHAPS::print("Unknown"); break;
     }
-    UTHAPS::print("  ");
+    Serial.print("  ");
     UTHAPS::println("M1 >> ", canMasterToIF.receive_state, "M2 >> ", canMasterToCenter.receive_state, "IF >> ", canIFtoMaster.receive_state, "LEFT >> ", canLeftToMaster.receive_state, "CENTER >> ", canCenterToMaster.receive_state, "RIGHT >> ", canRightToMaster.receive_state);
     
     
     // RESET
-    // canMasterToIF.receive_state = false;
-    // canMasterToCenter.receive_state = false;
-    // canIFtoMaster.receive_state = false;
-    // canLeftToMaster.receive_state = false;
-    // canCenterToMaster.receive_state = false;
-    // canRightToMaster.receive_state = false;
+    canMasterToIF.receive_state = false;
+    canMasterToCenter.receive_state = false;
+    canIFtoMaster.receive_state = false;
+    canLeftToMaster.receive_state = false;
+    canCenterToMaster.receive_state = false;
+    canRightToMaster.receive_state = false;
     // delay(1000);
 }

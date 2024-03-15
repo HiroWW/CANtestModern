d46fd31e5c41f7b3627fdfc08182514f50e9043a
hiroaki <hiroaki.tokyu8500@gmail.com>
2024-03-15 16:32:33 +0900

    feat : てすと

--- src/main.cpp
+++ src/main.cpp
@@ -81,112 +81,12 @@ void setup() {
     Serial7.addMemoryForWrite(extra_write_buffer_7, extra_buffer_size);
     Serial8.addMemoryForWrite(extra_write_buffer_8, extra_buffer_size);
     Serial.println("Waiting for setup...");
-    canpack.CANsetup();
-    
+    canpack.CANsetup(nullptr, 0, 40, 1024, true);
     Serial.println("CAN setup : COMPLETE");
 }
 
 int loopCount = 0;
 void loop() {
-    // フラグに応じた処理
-    // Serial.println(micros());
-    uint32_t startTime = micros();
-    while (micros() - startTime < 15000){
-    }
-    // Serial.println(micros());
-    switch (CURRENT_FLAG) {
-        case MASTER1:
-            // MASTER1の場合の処理
-            // Serial.println("Executing MASTER1 logic...");
-            canMasterToIF.receiveState = 1;
-            canMasterToRight.deCommand = 1111.f * (loopCount%10);
-            // canpack.CANsend(CAN_ID_MASTERTOIF, &canMasterToIF);
-            canpack.CANsend(CAN_ID_MASTERTORIGHT, &canMasterToRight);
-            canpack.CANread({CAN_ID_MASTERTOCENTER, CAN_ID_IFTOMASTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
-            break;
-        case MASTER2:
-            // MASTER2の場合の処理
-            // Serial.println("Executing MASTER2 logic...");
-            canMasterToCenter.receiveState = 1;
-            canpack.CANsend(CAN_ID_MASTERTOCENTER, &canMasterToCenter);
-            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_IFTOMASTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
-            break;
-        case IF:
-            // IFの場合の処理
-            // Serial.println("Executing IF logic...");
-            canIFtoMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_IFTOMASTER,&canIFtoMaster);
-            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
-            break;
-        case LEFT:
-            // LEFTの場合の処理
-            // Serial.println("Executing LEFT logic...");
-            // canLeftToMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_LEFTTOMASTER, &canLeftToMaster);
-            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_IFTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_RIGHTTOMASTER});
-            break;
-        case CENTER:
-            // CENTERの場合の処理
-            // Serial.println("Executing CENTER logic...");
-            // canCenterToMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_CENTERTOMASTER, &canCenterToMaster);
-            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_IFTOMASTER, CAN_ID_RIGHTTOMASTER});
-            break;
-        case RIGHT:
-            // RIGHTの場合の処理
-            // Serial.println("Executing RIGHT logic...");
-            // canRightToMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_RIGHTTOMASTER, &canRightToMaster);
-            canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTORIGHT, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_CENTERTOMASTER, CAN_ID_IFTOMASTER});
-            
-            break;
-        case DEBUG:
-            // DEBUG MODE FOR 2 BOARDS COM
-            // SEND M1 M2 IF L C 
-            canMasterToIF.receiveState = 1;
-            canpack.CANsend(CAN_ID_MASTERTOIF, &canMasterToIF);
-            canMasterToCenter.receiveState = 1;
-            canpack.CANsend(CAN_ID_MASTERTOCENTER, &canMasterToCenter);
-            canIFtoMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_IFTOMASTER,&canIFtoMaster);
-            // canLeftToMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_LEFTTOMASTER, &canLeftToMaster);
-            // canCenterToMaster.receiveState = 1;
-            canpack.CANsend(CAN_ID_CENTERTOMASTER, &canCenterToMaster);
-
-            canpack.CANread({CAN_ID_RIGHTTOMASTER});
-            break;
-        default:
-            // 定義されていないフラグの場合の処理
-            Serial.println("Unknown flag!");
-            break;
-    }
-
-    // 現在のフラグをシリアルで出力
-    if (loopCount % 1 == 0){
-
-        UTHAPS::println("=======================================================================================================================================");
-        // 現在のフラグの名前をシリアルで出力
-        Serial.print("Current flag is: ");
-        switch (CURRENT_FLAG) {
-            case MASTER1: Serial.print("MASTER1"); break;
-            case MASTER2: Serial.print("MASTER2"); break;
-            case IF: Serial.print("IF"); break;
-            case LEFT: Serial.print("LEFT"); break;
-            case CENTER: Serial.print("CENTER"); break;
-            case RIGHT: Serial.print("RIGHT"); break;
-            case DEBUG: Serial.print("DEBUG"); break;
-            default: Serial.print("Unknown"); break;
-        }
-        Serial.print("  ");
-        // UTHAPS::println("M1 >> ", canMasterToRight.updateTime, "M2 >> ", canMasterToCenter.receive_state, "IF >> ", canIFtoMaster.receive_state, "LEFT >> ", canLeftToMaster.receive_state, "CENTER >> ", canCenterToMaster.receive_state, "RIGHT >> ", canRightToMaster.receive_state);
-        
-    }
-    loopCount ++;
-    
-    // RESET
-    canMasterToIF.receiveState = false;
-    canMasterToCenter.receiveState = false;
-    canIFtoMaster.receiveState = false;
-    // delay(1000);
+    canpack.CANread(2, &canCenterToMaster);
+    canpack.CANread({CAN_ID_MASTERTOIF, CAN_ID_MASTERTOCENTER, CAN_ID_LEFTTOMASTER, CAN_ID_IFTOMASTER, CAN_ID_RIGHTTOMASTER});
 }
